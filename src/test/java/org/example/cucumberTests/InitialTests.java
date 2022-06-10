be4ebd77f1dacf869a7e1b0c2307d4ba393f23d0
demyanova-elizaveta <72383419+demyanova-elizaveta@users.noreply.github.com>
2022-06-10 20:38:08 +0300

    Delete src/test/java/org/example/cucumberTests directory
    deleted an unnecessary directory

--- src/test/java/org/example/cucumberTests/InitialTests.java
+++
@@ -1,8 +0,0 @@
-package org.example.cucumberTests;
-
-import io.cucumber.testng.AbstractTestNGCucumberTests;
-import io.cucumber.testng.CucumberOptions;
-
-@CucumberOptions(features = {"src/test/java/org/example/features"}, glue = {"ru.savkk.test"})
-public class InitialTests extends AbstractTestNGCucumberTests {
-}
